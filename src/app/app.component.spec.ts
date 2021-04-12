1a3d01ad0b6e4bd98f3699cfd8ac427284d82725
moreiraKevellyn <moreirakevellyn@gmail.com>
2021-04-12 19:19:07 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+
+import { MockComponents } from 'ng-mocks';
+import { MatSidenavModule, MatSidenavContainer, MatSidenav } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { RouterTestingModule } from '@angular/router/testing'
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+
+      imports: [
+        RouterTestingModule
       ],
+
+      declarations: [
+        AppComponent,
+        MockComponents(
+          MatSidenavModule,
+          MatIconModule,
+          MatToolbarModule,
+          MatSidenavContainer,
+          MatSidenav
+        )
+      ],
+      
     }).compileComponents();
   });
 
@@ -16,16 +36,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'petstore'`, () => {
+  it(`should have as title 'Petstore'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('petstore');
+    expect(app.title).toEqual('Petstore');
   });
 
-  it('should render title', () => {
+  it('should render title in mat-toolbar', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('petstore app is running!');
+    expect(compiled.querySelector('mat-sidenav-content span').textContent).toContain('Petstore');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
